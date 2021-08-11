9f6a34bbf49cc52a5c3ab145d8ac6d3ab61c2d9a
Brian Hogg <brian@hoggworks.com>
2021-08-11 13:39:01 -0400

    Initial

---
+++ schemas/social.js
@@ -0,0 +1,25 @@
+export default {
+  name: 'social',
+  title: 'Social Link',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'link',
+      title: 'link',
+      type: 'url',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+}
